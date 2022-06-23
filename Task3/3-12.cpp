1bceb1886d49109aadb61e498199a900895471d0
Ishirai <kirakiseki@163.com>
2022-06-23 18:04:06 +0800

    feat: Add Task3-12

--- Task3/3-12.cpp
+++ Task3/3-12.cpp
@@ -40,13 +40,20 @@ public:
     Box(double _x1, double _y1, double _z1, double _x2, double _y2, double _z2)
             : Rectangle(*new Point(_x1, _y1, _z1),
                         *new Point(_x2, _y2, _z1)),
-              p(_x2, _y2, _z2), h(abs(getPoint1().getZ() - p.getZ())) {}
+              p(_x2, _y2, _z2), h(getPoint2().getZ() - p.getZ()) {}
+
+    Box(const Box &b) : Rectangle(b.getPoint1(), b.getPoint2()),
+                        p(b.getP()), h(b.getH()) {}
 
     double surface() const;
 
     double volume() const;
 
-    bool isBox() const { return h > 0; };
+    bool isBox() const { return h != 0; };
+
+    Point getP() const { return p; };
+
+    double getH() const { return abs(h); };
 
 private:
     Point p;
@@ -54,13 +61,13 @@ private:
 };
 
 double Box::volume() const {
-    return Rectangle::area() * h;
+    return Rectangle::area() * abs(h);
 }
 
 double Box::surface() const {
     return 2 * (Rectangle::area() +
-                h * abs(getPoint1().getX() - getPoint2().getX()) +
-                h * abs(getPoint1().getY() - getPoint2().getY()));
+                abs(h) * abs(getPoint1().getX() - getPoint2().getX()) +
+                abs(h) * abs(getPoint1().getY() - getPoint2().getY()));
 }
 
 bool Rectangle::isRectangle() const {
@@ -75,13 +82,25 @@ int main() {
     double x1, y1, x2, y2, z1, z2;
     cout << "Please input two points: x1,y1,z1,x2,y2,z2." << endl;
     cin >> x1 >> y1 >> z1 >> x2 >> y2 >> z2;
-    Box b(x1, y1, z1, x2, y2, z2);
-    if (b.isRectangle() && b.isBox()) {
-        cout << "Bottom Area = " << b.area() << endl;
-        cout << "Surface area = " << b.surface() << endl;
-        cout << "Volume = " << b.volume() << endl;
+    Box b1(x1, y1, z1, x2, y2, z2);
+    cout << "Box b1:" << endl;
+    if (b1.isRectangle() && b1.isBox()) {
+        cout << "Bottom Area = " << b1.area() << endl;
+        cout << "Surface area = " << b1.surface() << endl;
+        cout << "Volume = " << b1.volume() << endl;
     } else {
-        cout << "This is not a valid box." << endl;
+        cout << "b1 is not a valid box." << endl;
     }
+    Box b2(b1);
+    cout << "create b2 using copy constructor." << endl;
+    cout << "Box b2:" << endl;
+    if (b2.isRectangle() && b2.isBox()) {
+        cout << "Bottom Area = " << b2.area() << endl;
+        cout << "Surface area = " << b2.surface() << endl;
+        cout << "Volume = " << b2.volume() << endl;
+    } else {
+        cout << "b2 is not a valid box." << endl;
+    }
+
     return 0;
 }
